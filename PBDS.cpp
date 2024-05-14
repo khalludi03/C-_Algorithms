1c14d6ad7bdf1ba7492df32bf334835b46931be1
Khaled Khalludi <102298889+khaled03121@users.noreply.github.com>
2024-05-14 15:55:54 +0600

    Create PBDS.cpp

---
+++ PBDS.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h> 
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace std; 
+using namespace __gnu_pbds;
+typedef tree<int,null_type,less<int>,rb_tree_tag,tree_order_statistics_node_update> ordered_set1;
+typedef tree<int,null_type,greater<int>,rb_tree_tag,tree_order_statistics_node_update> ordered_set2;
+typedef tree<pair<int,int>,null_type,less<pair<int,int>>,rb_tree_tag,tree_order_statistics_node_update> ordered_set_s;
+typedef tree<pair<int,int>,null_type,less_equal<pair<int,int>>,rb_tree_tag,tree_order_statistics_node_update> ordered_set_s2;
+
+int main()  
+{ 
+    
+
+    return 0;
+}
